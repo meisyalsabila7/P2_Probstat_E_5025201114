eaa6c0870eb05ff660d642bda6b765fecbfcbe2e
meisyalsabila7 <94627623+meisyalsabila7@users.noreply.github.com>
2022-05-29 19:54:29 +0700

    Update E_5025201114.R

--- E_5025201114.R
+++ E_5025201114.R
@@ -1,1 +1,54 @@
+# Soal 1
+# 1a. Carilah Standar Deviasi dari data selisih pasangan pengamatan tabel diatas
+
+# Data Sebelum dan Sesudah melakukan aktivitas
+orang.ke = c(seq(1:9))
+sebelum = c(78, 75, 67, 77, 70, 72, 78, 74, 77)
+sesudah = c(100, 95, 70, 90, 90, 90, 89, 90, 100)
+data = data.frame(orang.ke,sebelum, sesudah)
+n = 9
+
+# Standar Deviasi
+selisih = data$sesudah - data$sebelum
+standardev = sd(selisih)
+standardev
+
+# 1b. Carilah nilai t (p-value)
+mu = 0
+
+# Cari rata rata selisihnya terlebih dahulu
+xbar = mean(selisih)
+xbar
+
+# Cari nilai t
+t =((xbar - mu) / (standardev/sqrt(n)))
+t
+
+# Cari p-value
+pvalue = 2 * pt(-abs(t), df=n-1)
+pvalue
+
+# 1c. Tentukanlah apakah terdapat pengaruh yang signifikan secara statistika dalam hal kadar saturasi oksigen , sebelum dan sesudah melakukan aktivitas 𝐴 jika diketahui tingkat signifikansi 𝛼 = 5% serta H0 : “tidak ada pengaruh yang signifikan secara statistika dalam hal kadar saturasi oksigen , sebelum dan sesudah melakukan aktivitas 𝐴”
+t.test(x=data$sesudah, 
+       y=data$sebelum,
+       alternative = "two.sided", 
+       mu = 0, 
+       paired = TRUE, 
+       var.equal = TRUE, 
+       conf.level = 0.95)
+
+
+# Soal 2
+standardev = 3900
+n = 100
+xbar = 23500
+mu = 20000
+
+# Cari p-value
+z =( (xbar - mu) / (standardev / sqrt(n)))
+pvalue = pnorm(-abs(z))
+pvalue
+
+# 2a, b, c
+# Jawaban di README
 
